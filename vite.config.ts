9f4451ed5be59d2bbadd30ae5703a7ba307e0fef
沈孟平 <mengping.shen@palmpay-inc.com>
2025-12-19 10:05:43 +0800

    feat: linter and formatter with biome

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { tanstackRouter } from "@tanstack/router-plugin/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-import { compression } from "vite-plugin-compression2";
-import svgr from "vite-plugin-svgr";
-import { fileURLToPath } from "node:url";
-import unoCSS from "unocss/vite";
+import { fileURLToPath } from 'node:url';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
+import react from '@vitejs/plugin-react';
+import unoCSS from 'unocss/vite';
+import { defineConfig } from 'vite';
+import { compression } from 'vite-plugin-compression2';
+import svgr from 'vite-plugin-svgr';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-const envDir = fileURLToPath(new URL("env", import.meta.url));
+const envDir = fileURLToPath(new URL('env', import.meta.url));
 
 export default defineConfig({
   envDir,
   plugins: [
-    tanstackRouter({ target: "react", autoCodeSplitting: true }),
-    react({ babel: { plugins: [["babel-plugin-react-compiler"]] } }),
+    tanstackRouter({ target: 'react', autoCodeSplitting: true }),
+    react({ babel: { plugins: [['babel-plugin-react-compiler']] } }),
     tsconfigPaths({}),
-    compression({ algorithms: ["brotli", "gzip"] }),
+    compression({ algorithms: ['brotli', 'gzip'] }),
     svgr(),
     unoCSS(),
   ],
