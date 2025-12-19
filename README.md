f0e15e385e274ae93fe8d5f4edbb044c8f10c737
沈孟平 <mengping.shen@palmpay-inc.com>
2025-12-19 08:50:45 +0800

    chore: init

---
+++ README.md
@@ -0,0 +1,9 @@
+<!-- "*": "npm run lint:fix"
+  "lint-staged": {}
+    "lint": "eslint .",
+
+    ,
+  "simple-git-hooks": {
+    "pre-commit": "npx lint-staged"
+  }
+    "lint:fix": "eslint . --fix", -->
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { compression } from "vite-plugin-compression2";
+import svgr from "vite-plugin-svgr";
+import { fileURLToPath } from "node:url";
+import unoCSS from "unocss/vite";
+
+const envDir = fileURLToPath(new URL("env", import.meta.url));
+
+export default defineConfig({
+  envDir,
+  plugins: [
+    tanstackRouter({ target: "react", autoCodeSplitting: true }),
+    react({ babel: { plugins: [["babel-plugin-react-compiler"]] } }),
+    tsconfigPaths({}),
+    compression({ algorithms: ["brotli", "gzip"] }),
+    svgr(),
+    unoCSS(),
+  ],
+});
