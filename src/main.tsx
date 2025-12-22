ef3064812c14bebd962f96c5343d44b405978a5d
沈孟平 <mengping.shen@palmpay-inc.com>
2025-12-22 11:12:12 +0800

    feat: add @tanstack/react-query dependency and integrate QueryClientProvider

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,15 @@ import '@/styles/global.scss';
 import { RouterProvider } from '@tanstack/react-router';
 import { router } from './router';
 import '@/locales';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 // biome-ignore lint/style/noNonNullAssertion: always have
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 );
